767450fedf047b1b583122f55369333f6c66b250
sianesantoso <siane.santoso10@gmail.com>
2025-02-20 22:41:39 +0700

    Remove all tracked files

--- buat_pw.py
+++
@@ -1,9 +0,0 @@
-import bcrypt
-
-password = "admin123"  # Ganti dengan password yang ingin di-hash
-hashed_password = bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
-
-# Konversi hasil hash dari bytes ke string
-hashed_password_str = hashed_password.decode('utf-8')
-
-print("Hash Password (String):", hashed_password_str)
--- start_app.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-cd /d "%~dp0"
-call new_env\Scripts\activate
-start /b python app.py
-start http://127.0.0.1:5000
